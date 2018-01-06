02d242e28d14bfc1c29a7eaf7020a7d64b0eaaaf
Bianca Bossé <bibi_bosse22@hotmail.com>
2018-01-05 20:45:28 -0500

    Add start and debug script Add eslint rules Fix the code to follow the eslint rules Add querystring to resquest

--- index.js
+++ index.js
@@ -1,69 +1,95 @@
 ﻿const request = require("request-promise-native");
 const _ = require("lodash");
+const LEGO_BASE_URL = "https://shop.lego.com";
+const OAUTH_URL = "https://shop.lego.com";
+const PRODUCT_URL = "sh/rest/products/pab/elements";
+const DARK_GREEN = 28;
+
+let token;
 
 main();
 
+function defaultOptions() {
+	const options = {
+		baseUrl: LEGO_BASE_URL, // TODO data?
+		method: "GET",
+		qs: {
+			api_version: 1,
+		},
+		json: true,
+		resolveWithFullResponse: true,
+	};
+
+	if (token) {
+		_.deepMerge(options, { auth: { bearer: token } });
+	}
+
+	return options;
+}
+
+async function init() {
+	token = await request.defaults(defaultOptions())({
+		url: OAUTH_URL,
+		method: "POST",
+	});
+}
+
+async function getProductWithColor(color) {
+	const rawData = await request.defaults(defaultOptions())({
+		url: PRODUCT_URL, // TODO data?
+		qs: {
+			exact_color: color,
+			offset: 0,
+			limit: 53,
+		},
+	});
+	return rawData.elements;
+}
+
 async function main() {
 	try {
-		const token = await request.post({ // TODO getJSON?
-			url: "https://shop.lego.com/oauth/accessToken?api_version=1&accept_language=en-CA", // TODO data?
-			headers: {
-				//accept: "application/json",
-			},
-			json: true,
-		});
+		await init();
 
-		let authorization = "Bearer " + token.access_token;
+		const greenLegos = await getProductWithColor(DARK_GREEN);
 
-		const data = await request.get({ // TODO getJSON?
-			url: "https://shop.lego.com/sh/rest/products/pab/elements?exact_color=28&offset=0&limit=53&api_version=1&accept_language=en-CA", // TODO data?
-			headers: {
-				//accept: "application/json",
-				authorization,
-			},
-			json: true,
-		});
-
-		_(data.elements).map(function (element) {
-			let {name, price, element_id, design_id} = element;
-			if (/^PLATE (\d)X(\d)$/.exec(name)) {
-				let width = Number(RegExp.$1);
-				let length = Number(RegExp.$2);
-				let studs = width * length;
-				let price_per_stud = price / studs;
+		_(greenLegos).map((element) => {
+			const { name, price, element_id, design_id } = element;
+			if (/^PLATE (\d+)X(\d+)$/.exec(name)) {
+				const width = Number(RegExp.$1);
+				const length = Number(RegExp.$2);
+				const studs = width * length;
+				const price_per_stud = price / studs;
 				return {
-					//element_id,
-					//design_id,
-					//title,
+					// element_id,
+					// design_id,
+					// title,
 					width,
 					length,
 					price,
 					studs,
-					price_per_stud
+					price_per_stud,
 				};
-			} else if (/^CORNER PLATE (\d)X(\d)X(\d)$/.exec(name)) {
-				let thickness = Number(RegExp.$1);
-				let width = Number(RegExp.$2);
-				let length = Number(RegExp.$3);
-				let studs = thickness * (width + length - thickness);
-				let price_per_stud = price / studs;
+			} else if (/^CORNER PLATE (\d+)X(\d+)X(\d+)$/.exec(name)) {
+				const thickness = Number(RegExp.$1);
+				const width = Number(RegExp.$2);
+				const length = Number(RegExp.$3);
+				const studs = thickness * (width + length - thickness);
+				const price_per_stud = price / studs;
 				return {
-					//element_id,
-					//design_id,
-					//title,
-					//thickness,
+					// element_id,
+					// design_id,
+					// title,
+					// thickness,
 					width,
 					length,
 					price,
 					studs,
-					price_per_stud
+					price_per_stud,
 				};
 			}
-		}).compact().sort((a, b) => (
-			a.price_per_stud - b.price_per_stud
-		) || (
-			b.studs - a.studs
-		)).each(console.log).reduce(([price_sum, studs_sum], {price, studs}) => [
+		}).compact().sort((a, b) => (a.price_per_stud - b.price_per_stud) || (b.studs - a.studs)
+		).each(console.log
+		).reduce(([price_sum, studs_sum], { price, studs }) => [
 			price_sum + price,
 			studs_sum + studs,
 		], [0, 0]).reduce((price_sum, studs_sum) => (price_sum / studs_sum));
